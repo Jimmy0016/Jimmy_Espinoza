229542da08626937a18d3f9b3b526c7f4ae5ce4a
Jimmy Espinoza <jrn.espinoza@yavirac.edu.ec>
2025-10-22 15:22:50 -0500

    primera parte

---
+++ app.py
@@ -0,0 +1,13 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Resultados")
+    x = float(input("Ingresa el primer número: "))
+    y = float(input("Ingresa el segundo número: "))
+
+    print(f"Suma: {sumar(x, y)}")
+    print(f"Resta: {restar(x, y)}")
